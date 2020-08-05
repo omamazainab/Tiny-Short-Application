52f38409fb967585adb94479ee27567542f9137c
Omama Zainab <omi.zainy@gmail.com>
2020-08-05 20:00:33 +0500

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
+import logo from '../assets/mutify-logo.png'
 
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg ">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand" href="/"><img src={logo} alt="logo" className='mr-4' />MUTIFY</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    
-                </ul>
-                
-            </div>
-        </nav>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item ">
+                            <a className="nav-link" href="/">Works With </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Noise Level</a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link" href="/">Switch Device </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">feedback</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
 
